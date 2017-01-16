71c2bf3f01bd4bf103c2b03c2d93677f4102947e
vladica1984 <vladabeba84@gmail.com>
2017-01-16 20:04:44 +0100

    Uradjen zadatak kreiranja tabela

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -2,6 +2,10 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import javafx.scene.control.Tab;
+import model.Knjiga;
+import model.Oblast;
 
 /**
  * Created by androiddevelopment on 16.1.17..
@@ -11,6 +15,12 @@ public class Zadatak1KreiranjeTabela {
         ConnectionSource connectionSource = null;
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+
+            TableUtils.dropTable(connectionSource, Oblast.class, true);
+            TableUtils.dropTable(connectionSource, Knjiga.class, true);
+
+            TableUtils.createTable(connectionSource, Knjiga.class);
+            TableUtils.createTable(connectionSource, Oblast.class);
 
         } catch (Exception e) {
             e.printStackTrace();
